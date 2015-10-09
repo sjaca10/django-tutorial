9e4a83c381730bbbf33f9d00e14f33f63a65b312
Javier Aguila <sjaca10@gmail.com>
2015-10-09 13:51:03 -0500

    Tutorial Part 2 Finished

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -1,13 +1,39 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 
 # Provides a pre-build/scafold from Question Model
 # admin.site.register(Question)
 
+# class ChoiceInline(admin.StackedInline): like a stack
+# Like a table
+class ChoiceInline(admin.TabularInline):
+    # The model to show in a inline view
+    model = Choice
+
+    # Number of inline choices to show
+    extra = 3
+
 # Provides a custom Question Model
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    # Set of data to show and how to show
+    fieldsets = [
+        (None,  {'fields' : ['question_text']}),
+        ('Date information', {'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+
+    # Selection in an inline view
+    inlines = [ChoiceInline]
+
+    # Display of data to show about Question
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    # Adding filter to the view
+    list_filter = ['pub_date']
+
+    # Search capability
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
